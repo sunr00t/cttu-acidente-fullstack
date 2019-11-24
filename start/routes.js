3275acb4823cbf2a1310580a99a72cdd96088076
sunr00t <sunr00t@gmail.com>
2019-11-24 19:36:41 -0300

    API CRUD

--- start/routes.js
+++ start/routes.js
@@ -1,19 +1,25 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+
+Route.group(() => {
+/*
+* Rota de Sessao
+*/
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+
+/*
+* Rota de Acidentes
+*/
+
+Route.post('/acidentes', 'AcidenteController.store')// Criar Acidente
+Route.get('/acidentes', 'AcidenteController.index')//Exibir todos acidentes
+Route.get('/acidentes/:id', 'AcidenteController.show') //Exibir por id
+Route.put('/acidentes/:id', 'AcidenteController.update') //Update Acidente
+Route.delete('/acidentes/:id', 'AcidenteController.destroy') //Delete Acidente
+
+}).prefix('/api')
