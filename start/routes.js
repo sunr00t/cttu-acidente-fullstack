fe1e83eb4b8be8f05a2fdf6ce66731bc8d8ca630
sunr00t <sunr00t@gmail.com>
2019-11-29 17:10:41 -0300

    change of plans vuejs client removed!

---
+++ start/routes.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+// Rotas de Client
+Route.get('/', ({ view }) =>{
+  return view.render('welcome')
+})
+
+Route.group( () =>{
+// Rotas de Sessao
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+
+// Rotas de Acidentes
+Route.post('acidentes', 'AcidenteController.store')// Criar Acidente
+Route.get('acidentes', 'AcidenteController.index')//Exibir todos acidentes
+Route.get('acidentes/:id', 'AcidenteController.show') //Exibir por id
+Route.put('acidentes/:id', 'AcidenteController.update') //Update Acidente
+Route.delete('acidentes/:id', 'AcidenteController.destroy') //Delete Acidente
+
+}).prefix('api/')
