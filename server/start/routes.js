2ce59c684d12703c9da4641c43ead46fb2eb2d13
sunr00t <sunr00t@gmail.com>
2019-11-29 12:07:04 -0300

    remove api prefix

--- server/start/routes.js
+++ server/start/routes.js
@@ -7,7 +7,7 @@ Route.get('/', () =>{
   return 'cttu-api-service started: receiving requests! read the documentation at https://documenter.getpostman.com/view/3856777/SVzxZL5G?version=latest'
 })
 
-Route.group(() => {
+
 /*
 * Rota de Sessao
 */
@@ -23,5 +23,3 @@ Route.get('/acidentes', 'AcidenteController.index')//Exibir todos acidentes
 Route.get('/acidentes/:id', 'AcidenteController.show') //Exibir por id
 Route.put('/acidentes/:id', 'AcidenteController.update') //Update Acidente
 Route.delete('/acidentes/:id', 'AcidenteController.destroy') //Delete Acidente
-
-}).prefix('/api')
