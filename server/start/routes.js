51142e419635eb47237ee53e57e8e8451eef4106
sunr00t <sunr00t@gmail.com>
2019-11-29 11:44:50 -0300

    Disable welcome render

--- server/start/routes.js
+++ server/start/routes.js
@@ -3,7 +3,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+// Route.on('/').render('welcome')
 
 Route.group(() => {
 /*
